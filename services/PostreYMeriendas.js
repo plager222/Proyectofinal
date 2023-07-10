d01a61a8e74063143365b9cb754808db29bf8c95
plager222 <118579251+plager222@users.noreply.github.com>
2023-07-10 08:26:18 -0300

    Create PostreYMeriendas.js

---
+++ services/PostreYMeriendas.js
@@ -0,0 +1,74 @@
+import styles from "./PostreYMeriendas.module.css";
+const PostreYMeriendas = () => {
+  return (
+    <div className={styles.postreYMeriendas}>
+      <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
+      <div className={styles.postresYMeriendas}>POSTRES Y MERIENDAS</div>
+      <div className={styles.almuerzoYCena}>ALMUERZO Y CENA</div>
+      <div className={styles.promocionesDelDia}>PROMOCIONES DEL DIA</div>
+      <img className={styles.image3Icon} alt="" src="/image-3@2x.png" />
+      <div className={styles.delivery}>DELIVERY</div>
+      <div className={styles.comoContactarnos}>COMO CONTACTARNOS</div>
+      <img className={styles.image4Icon} alt="" src="/image-4@2x.png" />
+      <img className={styles.image5Icon} alt="" src="/image-5@2x.png" />
+      <div className={styles.div}>{`11 5586 2347 `}</div>
+      <div className={styles.kids}>KIDS</div>
+      <img className={styles.image8Icon} alt="" src="/image-8@2x.png" />
+      <img className={styles.image10Icon} alt="" src="/image-10@2x.png" />
+      <img className={styles.image11Icon} alt="" src="/image-11@2x.png" />
+      <img className={styles.image12Icon} alt="" src="/image-12@2x.png" />
+      <img className={styles.image13Icon} alt="" src="/image-13@2x.png" />
+      <img className={styles.image14Icon} alt="" src="/image-14@2x.png" />
+      <img className={styles.image15Icon} alt="" src="/image-15@2x.png" />
+      <img className={styles.image16Icon} alt="" src="/image-16@2x.png" />
+      <img className={styles.image17Icon} alt="" src="/image-17@2x.png" />
+      <img className={styles.image18Icon} alt="" src="/image-18@2x.png" />
+      <img className={styles.image19Icon} alt="" src="/image-19@2x.png" />
+      <img className={styles.image48Icon} alt="" src="/image-48@2x.png" />
+      <div className={styles.nutraviandas}>@Nutra.viandas</div>
+      <div className={styles.tortaDeFrutillaContainer}>
+        <p className={styles.tortaDeFrutilla}>Torta de frutilla</p>
+        <p className={styles.tortaDeFrutilla}>$ 700,00</p>
+      </div>
+      <div className={styles.sambayn70000Container}>
+        <p className={styles.tortaDeFrutilla}>Sambayón</p>
+        <p className={styles.tortaDeFrutilla}>$ 700,00</p>
+      </div>
+      <div className={styles.panquequeDeDulceContainer}>
+        <p className={styles.tortaDeFrutilla}>Panqueque de dulce de leche</p>
+        <p className={styles.tortaDeFrutilla}>$ 700,00</p>
+      </div>
+      <div className={styles.mousseDeBananaContainer}>
+        <p className={styles.tortaDeFrutilla}>Mousse de banana</p>
+        <p className={styles.tortaDeFrutilla}>$ 700,00</p>
+      </div>
+      <div className={styles.mokaccino70000Container}>
+        <p className={styles.tortaDeFrutilla}>Mokaccino</p>
+        <p className={styles.tortaDeFrutilla}>$ 700,00</p>
+      </div>
+      <div className={styles.suspiroDeFrutillaContainer}>
+        <p className={styles.tortaDeFrutilla}>Suspiro de frutilla</p>
+        <p className={styles.tortaDeFrutilla}>$ 700,00</p>
+      </div>
+      <div className={styles.mximoDeArndanosContainer}>
+        <p className={styles.tortaDeFrutilla}>Máximo de arándanos</p>
+        <p className={styles.tortaDeFrutilla}>$ 700,00</p>
+      </div>
+      <div className={styles.manzanaAsadaContainer}>
+        <p className={styles.tortaDeFrutilla}>Manzana asada</p>
+        <p className={styles.tortaDeFrutilla}>$ 700,00</p>
+      </div>
+      <div className={styles.manjar70000Container}>
+        <p className={styles.tortaDeFrutilla}>Manjar</p>
+        <p className={styles.tortaDeFrutilla}>$ 700,00</p>
+      </div>
+      <div className={styles.mousseDeChocolateContainer}>
+        <p className={styles.tortaDeFrutilla}>Mousse de chocolate</p>
+        <p className={styles.tortaDeFrutilla}>$ 700,00</p>
+      </div>
+    </div>
+  );
+};
+
+
+export default PostreYMeriendas;
